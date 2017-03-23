8a3dc19f1112303592bc410880a151d0fec83d67
jped <jonathanped@gmail.com>
2017-03-22 21:58:14 -0400

    added new essay 2017-03-22

--- thoughts.py
+++ thoughts.py
@@ -1,26 +1,30 @@
 import os
 import datetime
-directory   = r"C:\Users\pedoe\Documents\code\personalsite\thoughts\\"
-html_dir    = r"C:\Users\pedoe\Documents\code\personalsite\thtml\\"
+from subprocess import call
+
+directory   = r"/mnt/c/Users/pedoe/Documents/code/personalsite/thoughts"
+html_dir    = r"/mnt/c/Users/pedoe/Documents/code/personalsite/thtml/"
 files       = os.listdir(directory)
+readable_date = str(datetime.date.today())
 for file in files:
     thought     = open(directory+ file,"r")
     contents    = thought.read().replace("\n", "<br>")
     name        = thought.name
-    name        = name[name.rfind("\\")+1:]
+    name        = name[name.rfind("/")+1:]
+    name        = name.split(".txt")[0]
     print name
     print "______________________________"
     thought.close()
     #create a thought html
     print directory+name
-    thtml       = open(html_dir+name.replace(".txt",".html"), "w")
+    thtml       = open(html_dir+name+".html", "w")
     thtml.write("""<doctype !html>
     <html>
     <head>
       <meta charset="utf-8">
       <title> Thoughts Jonathan Pedoeem</title>
-      <link rel="stylesheet" href="./css/bootstrap.css" type="text/css">
-      <link rel="stylesheet" href="./css/styles.css" type="text/css">
+      <link rel="stylesheet" href="../css/bootstrap.css" type="text/css">
+      <link rel="stylesheet" href="../css/styles.css" type="text/css">
     </head>
     <body class= "flexCenter">
       <div class = "container">
@@ -31,7 +35,7 @@ for file in files:
       </div>
     </body>
     </html>
-""".format(name,str(datetime.date.today()), contents))
+""".format(name,readable_date, contents))
     thtml.close()
     #delete the thought file
     os.remove(directory+ file)
@@ -39,9 +43,10 @@ for file in files:
 html_files       = os.listdir(html_dir)
 html_list        = "<ul>"
 for hf in html_files:
-    html_list+="<li><a href='./thml/{0}''>{1}</a></li>".format(hf, hf[:hf.find('.')])
+    html_list+="<li><a href='./thml/{0}'>{1}</a></li>".format(hf, hf[:hf.find('.')])
 
 html_list+= "</ul>"
+print html_list
 thought_html   =   """<doctype !html>
 <html>
 <head>
@@ -63,6 +68,10 @@ thought_html   =   """<doctype !html>
 </html>
 """.format(html_list)
 
-thought_file = open("thoughts.html", "w")
+thought_file = open("/mnt/c/Users/pedoe/Documents/code/personalsite/thoughts.html", "w")
 thought_file.write(thought_html)
 thought_file.close()
+
+call("git add --all", shell=True   )
+call("git commit -m 'added new essay {0}'".format(readable_date), shell=True)
+call('git push', shell=True)
